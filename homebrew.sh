6fd422e934f1c9b4e76e483cd94ec7c9838dfd91
Kenny <1841819+kenny-f@users.noreply.github.com>
2019-11-07 10:50:09 +0000

    add jq

--- homebrew.sh
+++ homebrew.sh
@@ -1,5 +1,4 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew install thefuck
-# task warrior https://taskwarrior.org
-brew insatll task
+brew install jq
