13164c3008f9620cfbacf141d0ba32698bc8ef1c
Kenny <1841819+kenny-f@users.noreply.github.com>
2019-11-07 14:56:21 +0000

    fix google chrome canary

--- casks.sh
+++ casks.sh
@@ -11,7 +11,7 @@ brew cask install vlc
 brew cask install postman
 brew cask install itsycal
 brew cask install google-chrome
-brew cask install google-chrome-canary
+brew tap homebrew/cask-versions && brew cask install google-chrome-canary
 brew cask install google-backup-and-sync
 brew cask install licecap
 brew cask install telegram
